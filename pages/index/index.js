90c55af8fef023dcb6b6b45f6ce8b3874ac12e2e
xilali <telangpu@usa.com>
2019-09-28 09:08:24 +0800

    ruleForbidden got

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,5 +10,13 @@ Page({
     this.setData({
       inputValue: e.detail.value
     })
+  },
+  qryRuleForbidden() {
+    this.setData({
+      inputValue: "遮挡号牌，扣12分"
+    })
+  },
+  error(e) {
+    console.log(e.detail)
   }
 })
